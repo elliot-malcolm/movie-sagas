8c1796c9164df29ad35707f4e4cca7d6f2c9d63a
Elliot Malcolm <talcumalc@gmail.com>
2020-11-08 22:20:38 -0600

    details.router for SELECT movie

--- server/routes/details.router.js
+++ server/routes/details.router.js
@@ -1,17 +1,20 @@
-// const express = require('express');
-// const router = express.Router();
-// const pool = require('../modules/pool')
+const express = require('express');
+const router = express.Router();
+const pool = require('../modules/pool')
 
-// router.get('/:id', (req, res) => {
-//     console.log(req.body);
-//     const queryText = `SELECT * FROM "movies";`;
-                        // JOIN 
-//     pool.query(queryText, [req.params.id])
-//     .then(result => {
-//       res.send(result.rows);  
-//     }).catch(error => {
-//       console.log(error);
-//       res.sendStatus(500);
-//     });
-//   });
-// module.exports = router;
+router.get('/:id', (req, res) => {
+    console.log(req.body);
+    let movieId = req.params.id
+    const queryText = `SELECT * FROM "movies";;
+    SELECT * FROM "movies"
+    JOIN "movies_genres" ON "movies"."id" = "movies_genres"."movies_id"
+    JOIN "genres" ON "genres"."id" = "movies_genres"."genres_id"
+    WHERE "movies"."id" = $1;`;
+    pool.query(queryText, [movieId])
+    .then(result => {res.send(result.rows);})
+    .catch(error => {
+      console.log(error);
+      res.sendStatus(500);
+    });
+  });
+module.exports = router;
